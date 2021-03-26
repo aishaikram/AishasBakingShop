7e29ba87ecaf18b474e0807d1ca87cc72877f607
Aisha Ikram <aisha.ikram@gmail.com>
2021-03-26 12:31:40 +0000

    Add project files.

---
+++ Controllers/PieController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AishasBakingShop.Models;
+using AishasBakingShop.ViewModels;
+
+namespace AishasBakingShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        // the category and pie repositories are automatically injected whereever they are required by the ASP.net MVC framework after they are registered in Startup class for DI
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public ViewResult List()
+        {/*
+            //ViewBag.CurrentCategory = "cheese bag";
+            //builtin method from asp.net core
+            return View(_pieRepository.AllPies);
+            */
+            //added following cide replacing above code 
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            pieListViewModel.Pies = _pieRepository.AllPies;
+            pieListViewModel.CurrentCategory = "Fruit Pies";
+            return View(pieListViewModel);
+        }
+    }
+}
---
+++ ViewModels/PieListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AishasBakingShop.Models;
+
+namespace AishasBakingShop.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
