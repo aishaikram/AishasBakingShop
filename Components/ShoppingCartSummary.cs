cb78f6e8f16d26a20700176051f5ded723775135
Aisha Ikram <aisha.ikram@gmail.com>
2021-04-02 15:15:48 +0100

    Added ShoppingCartSummary and Category Menu Component, only ShoppingCartSummary works withouts icon. This checkpoint is before adding suppport for Font Awesome

---
+++ Components/ShoppingCartSummary.cs
@@ -0,0 +1,35 @@
+﻿using AishasBakingShop.Models;
+using AishasBakingShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AishasBakingShop.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+        //viewcomponent return type is IViewComponentResult which is specific to View Component
+        
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
